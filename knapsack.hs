1f856d7674e8568f0d3db56d8515b2c0941f399a
Sam Radhakrishnan <sk09idm@gmail.com>
2015-05-12 20:10:42 +0530

    Added Knapsack Problem Soulution

--- knapsack.hs
+++ knapsack.hs
@@ -3,3 +3,4 @@ knapsack c [] [] = 0
 knapsack c (x:xs) (y:ys) | c >= x = let (a, b) = ( y + knapsack (c-x) xs ys , knapsack c xs ys)
 							 	   in max a b
 knapsack c (x:xs) (y:ys) | otherwise = knapsack c xs ys
+	
